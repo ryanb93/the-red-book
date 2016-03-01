ae8fe236e5df1083bb5c3a772731d768a9002b6a
Ryan Burke <ryan.burke@digital.hmrc.gov.uk>
2016-03-01 13:03:22 +0000

    Cleaned up Cafe code

--- src/main/scala/chapter1/Cafe.scala
+++ src/main/scala/chapter1/Cafe.scala
@@ -14,18 +14,8 @@ case class Charge(cc: CreditCard, amount: Double) {
 }
 
 class Cafe {
-
   def coalesce(charges: List[Charge]): List[Charge] = charges.groupBy(_.cc).values.map(_.reduce(_ combine _)).toList
-
-  def buyCoffee(cc: CreditCard, cup: Coffee): Order = {
-    Order(List(cup), Charge(cc, cup.price))
-  }
-
-  def buyCoffee(cc: CreditCard, cups: List[Coffee]): Order = {
-    val orders = cups.map(cup => buyCoffee(cc, cup))
-    val charge = orders.map(_.charge).reduce((c1, c2) => c1.combine(c2))
-    Order(cups, charge)
-  }
-
+  def generateOrder(cc: CreditCard, cup: Coffee): Order = Order(List(cup), Charge(cc, cup.price))
+  def generateOrder(cc: CreditCard, cups: List[Coffee]): Order = Order(cups, cups.map(generateOrder(cc, _).charge).reduce(_ combine _))
 }
 
--- src/test/scala/chapter1/CafeSpec.scala
+++ src/test/scala/chapter1/CafeSpec.scala
@@ -9,14 +9,14 @@ class CafeSpec extends FlatSpec with Matchers {
 
   "A Cafe" should "buy a single cup of coffee" in {
     val cup = Coffee(3.50)
-    val order = cafe.buyCoffee(cc, cup)
+    val order = cafe.generateOrder(cc, cup)
     order.charge.amount shouldBe 3.50
     order.coffees.size shouldBe 1
   }
 
   it should "be able to buy multiple cups of coffee" in {
     val cups = List(Coffee(3.50), Coffee(4.00), Coffee(5.00))
-    val order = cafe.buyCoffee(cc, cups)
+    val order = cafe.generateOrder(cc, cups)
     order.charge.amount shouldBe 12.50
     order.coffees.size shouldBe 3
   }
